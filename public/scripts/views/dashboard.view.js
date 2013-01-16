cc4a2da205d6e2b0f32f9b26fe06c3162ccd43a3
Phillip Epstein <Phillip@core-tech.com>
2013-01-16 16:12:15 -0700

    added in inception

--- public/scripts/views/dashboard.view.js
+++ public/scripts/views/dashboard.view.js
@@ -1,16 +1,34 @@
 App.View.Dashboard = Backbone.View.extend({
 	
 	initialize: function(){
-		this.introView = new App.View.Intro();
+		this.introView = new App.View.Intro({
+			id: 'intro'
+		});
 	},
 	
 	build: function(){
 		var that = this;
-		return ['.main', [
+		
+		return ['.main',{ render: function(){
+			// var pageInception = new inception();
+// 	
+			// pageInception.$content = $('.main');
+		 	// pageInception.$nav = $('#nav');
+		
+		  	// var $body = $('body'), iOSclass = isIOS ? 'ios' : 'no-ios';
+// 		
+		  	// $body.addClass( iOSclass );
+		
+			// if ( Modernizr.csstransforms ) {
+				// $('.page-nav').each(function(){
+					// this.addEventListener( 'click', pageInception, false );
+				// });
+			  // }
+		}},[
 			this.introView.bind('done', function(){
 				$('.dark-tower', that.el).toggle();
 			}),
-			['section.dark-tower',[
+			['section.dark-tower',{ id:'darkTower' }, [
 				['section.hugo-stiglitz']
 			]]
 		]]
