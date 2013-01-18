2de9f1d92a56ce116fa3f0956d533ca510c0048a
oo7ph <Phillip@core-tech.com>
2013-01-18 01:34:27 -0700

    - removed garbage - Added 2 level inception

--- public/scripts/jscore/inception-parallax.js
+++ public/scripts/jscore/inception-parallax.js
@@ -1,3 +1,31 @@
-var inception = (function(){
+var inception = {
 	
-})();
+	init: function(content, scrolled, levels){
+		this.content = content;
+		this.scrolled = scrolled;
+		this.levels	= levels;
+		this.docHeight = document.documentElement.offsetHeight;
+		
+		window.addEventListener( 'scroll', this, false);
+	},
+	
+	handleEvent: function( event ) {
+		if ( this[event.type] ) {
+			this[event.type](event);
+		}
+	},
+	
+	scroll: function( event ) {
+		// normalize scroll value from 0 to 1
+		this.scrolled = window.scrollY / ( this.docHeight - window.innerHeight );
+		
+		var scale = Math.pow( 3, this.scrolled * this.levels );
+		var transformValue = 'scale('+scale+')';		
+		
+		this.content.style.WebkitTransform = transformValue;
+		this.content.style.MozTransform = transformValue;
+		this.content.style.OTransform = transformValue;
+		this.content.style.transform = transformValue;
+	},
+	
+}
--- public/scripts/views/dashboard.view.js
+++ public/scripts/views/dashboard.view.js
@@ -1,5 +1,7 @@
 App.View.Dashboard = Backbone.View.extend({
 	
+	className: 'main',
+	
 	initialize: function(){
 		this.introView = new App.View.Intro();
 	},
@@ -7,30 +9,35 @@ App.View.Dashboard = Backbone.View.extend({
 	build: function(){
 		var that = this;
 		
-		return ['.wrap',[
-			['.container', [
-				['ul.nav',[
-					//['li a', { href:'#intro' }, 'Phillip Epstein'],
-					//['li a', { href:'#who-is' }, 'Who Is He?'],
-				]],
-				['.content',[
-					['section.intro',[
-						this.introView.bind('done', function(){
-							$('.dark-tower', that.el).toggle();
-						}),
-						['test0']
+		return ['fragment', [
+			['.wrap .background', [
+				['.container', [
+					['ul.nav',[
+						//['li a', { href:'#intro' }, 'Phillip Epstein'],
+						//['li a', { href:'#who-is' }, 'Who Is He?'],
 					]],
-					['section.who-is',[
-						['.dark-tower .hugo-stiglitz']
+					['.content', { render: function(content){
+						inception.init(content, 0, 2);
+					} },[
+						['section.intro',[
+							this.introView.bind('done', function(){
+								$('.dark-tower', that.el).toggle();
+							}),
+							['test0']
+						]],
+						['section.who-is',[
+							['.dark-tower .hugo-stiglitz']
+						]],
+						['section.test1',[
+							['.test1']
+						]],
+						['section.test2',[
+							['.test2']
+						]],
 					]],
-					['section.test1',[
-						['.test1']
-					]],
-					['section.test2',[
-						['.test2']
-					]],
-				]],
-			]]
+				]]
+			]],
+			['.scroller']
 		]];
 	},
 });
